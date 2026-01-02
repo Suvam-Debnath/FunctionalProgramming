ca3375b670eb7d4a0d4b1c9df2bd57725adde28e
Suvam Debnath <debnathsuvam322@gmail.com>
2026-01-02 23:22:58 +0530

    using skip

---
+++ src/com/suvam/interviewQuestions/UsingSkip.java
@@ -0,0 +1,17 @@
+// Skip the first 4 elements and fetch the remaining elements from a list of integers
+
+package com.suvam.interviewQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsingSkip {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> result = numbers.stream()
+                .skip(4)
+                .collect(Collectors.toList());
+        result.forEach(System.out::println);
+    }
+}
